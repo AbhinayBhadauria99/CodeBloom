624182929a511c30eb60b2fd6ba9475cafef57c4
Abhinay S. Bhadauria <knowledgepoint887@gmail.com>
2024-06-25 22:48:03 +0530

    Responsiveness updated 2

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -4,7 +4,7 @@ function Cards() {
             <div className="cardcontainer h-screen w-1/2">
                 <div className="card relative rounded-xl w-[30vh] h-[20vh] lg:h-[40vh] lg:w-[50vh] bg-[#004D43] flex items-center justify-center">
                     <img className="w-32" src="https://ochi.design/wp-content/uploads/2022/04/logo002.svg"></img>
-                    <button className="absolute px-5 py-1 rounded-full border-2 left-10 bottom-10">&copy;2024</button>
+
                 </div>
             </div>
             <div className="cardcontainer flex items-center justify-center gap-5 w-1/2 h-[50vh]">
